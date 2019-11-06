7f79928ed30e557ebde012cb960b5c27f7a93514
Len Dierickx <len@astuanax.com>
2019-11-06 21:56:47 +0100

    Reduces complexity for gini

--- src/util/gini.js
+++ src/util/gini.js
@@ -12,24 +12,17 @@ import { convert2frequencies, countClasses } from './helpers'
  */
 const gini = new Impurity(
   matrix => {
-    const nrRows = matrix.getRows()
-
-    const arr = matrix.__value.map(row => row[row.length - 1])
-    const countedClasses = countClasses(arr)
-    // const countedClasses = arr.reduce((acc, val) => {
-    //   acc[val] = acc[val] === undefined ? 1 : acc[val] += 1
-    //   return acc
-    // }, {})
-    const classFrequencies = convert2frequencies(nrRows)(countedClasses)
+    const countedClasses = countClasses(matrix.getColumn(matrix.getCols() - 1))
+    const classFrequencies = convert2frequencies(matrix.getRows())(countedClasses)
 
     let imp = 0
     const len = classFrequencies.length
+
     for (let i = 0; i < len; ++i) {
       const p1 = classFrequencies[i]
+
       for (let j = 0; j < len; ++j) {
-        if (i === j) {
-          // skip
-        } else {
+        if (i !== j) {
           const p2 = classFrequencies[j]
           imp += p1 * p2
         }
